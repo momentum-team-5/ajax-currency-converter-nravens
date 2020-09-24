f4057265e50f68ea703eccd11639285368f2e814
Nathan Ravenscraft <nraven22@gmail.com>
2020-09-24 10:43:27 -0400

    used submit button as event listener

--- script.js
+++ script.js
@@ -34,42 +34,28 @@ const currencies = [
   'MYR'
 ]
 
-const contentDiv = document.getElementById('wrapper')
+
+
 const base_url = 'https://api.exchangeratesapi.io/latest?base=' 
-let exchange_rate
-let final_amount
-let currency_data
-let lookup_key
-
-
-
 const selectElement = document.querySelector('#first_currency')
-selectElement.addEventListener('change', (event) => {
-  const result = document.querySelector('.result')
-  let path = event.target.value  
-  console.log(path)
-  url = base_url + path
-  console.log(url)
+const selectElement2 = document.querySelector('#second_currency')
+const selectInput = document.querySelector('#number')
+const selectSubmit = document.querySelector('#submit') 
+const resultDiv = document.querySelector('.result')
 
-  fetch(url)
+
+selectSubmit.addEventListener('click', (event) => {
+  
+  fetch(`${base_url}${selectElement.value}`)
   .then (promise => promise.json()) 
   .then (function(data) {
-    currency_data = data
-    console.log(currency_data.rates)
-    console.log(currency_data.rates.USD)
-     })
+    //console.log(data)
+    //console.log(selectInput.value)
+    resultDiv.textContent = parseFloat(selectInput.value) * data.rates[selectElement2.value]
+  })
 })
 
-const selectElement2 = document.querySelector('#second_currency')
-selectElement2.addEventListener('change', (event) => {
-  const resultDiv = document.querySelector('.result')
-  let lookup_key = event.target.value  
-  console.log(lookup_key)
-  console.log(currency_data)
-  exchange_rate = currency_data['rates'][lookup_key] 
-  console.log(exchange_rate)
-  resultDiv.textContent = exchange_rate
-})
+
 
 
 
